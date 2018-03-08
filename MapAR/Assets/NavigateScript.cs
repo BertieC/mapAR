4fa1b7a188ec660885e3e9c2e683cf15e68c3c20
BertieC <coetzeealbertus@gmail.com>
2018-03-08 21:16:07 +0200

    adding test nav button

---
+++ MapAR/Assets/NavigateScript.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavigateScript : MonoBehaviour {
+    public void Navigate(string location) {
+        Application.OpenURL(location);
+    }
+}
